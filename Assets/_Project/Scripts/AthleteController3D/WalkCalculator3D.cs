bf3d9b8d14605001c93697de9fed667984fde0f3
Marucoge <airfromcountryderry@gmail.com>
2018-01-29 19:41:27 +0900

    GravityCalculator3D を作った。

---
+++ Assets/_Project/Scripts/AthleteController3D/WalkCalculator3D.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+//using UnityEngine.UI;
+//using UnityEditor;
+using UnityEngine;
+using System;
+
+
+namespace Labo{
+    public class WalkCalculator3D : IMovementCalculator3D {
+        public Vector3 MovementPerFrame { get; private set; }
+        
+
+        public void ManualUpdate() {
+
+        }
+    }
+}
